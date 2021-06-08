6644637b9d68c2d03b5bfe82c301e503cc52b287
Deepraj Chanda <30354189+iDeepraj10@users.noreply.github.com>
2021-06-08 22:33:34 +0530

    Indices of Nan values
    Array of customers with missing rating and respective services

---
+++ Master.py
@@ -0,0 +1,50 @@
+
+
+import pandas as pd
+import numpy as np
+from scipy import sparse
+from scipy.spatial import distance
+
+
+df = pd.read_csv( "C:\\Users\\dexter\\Desktop\\Trust and Reputation\\New folder\\Dataset\\matrix3.1.csv")
+df1=df
+
+
+
+
+df1= df1.fillna(0)
+#print(df1)
+
+
+#
+#def standardize(row):
+#    new_row = (row - row.mean())/(row.max()-row.min())
+#    return new_row
+
+#df_std = df1.apply(standardize)
+#print(df_std)
+
+
+
+#from sklearn.metrics.pairwise import cosine_similarity
+#from sklearn.metrics.pairwise import euclidean_distances
+#similarity= euclidean_distances(df_std)
+#print(similarity)
+
+
+
+#similarity_df = pd.DataFrame(similarity)
+#print(similarity_df)
+
+
+
+#def get_similar_consumer(c):
+#    sim_con= similarity_df[c]
+#    print(sim_con)
+#    print("similarity consumer : ")
+#    sim_con= sim_con.sort_values(ascending=True)
+#    return sim_con
+
+
+x = np.argwhere(np.isnan(np.array(df)))
+
--- matrix 2.py
+++ matrix 2.py
@@ -10,10 +10,10 @@ with open('matrix2.csv','w', newline='', encoding='utf-8') as f:
         col = [] 
         rate=1
         for j in range(30):
-      #      if(i==0):
-      #          col.append(rate)
-      #          rate=rate+1
-      #          continue
+            if(i==0):
+                col.append(rate)
+                rate=rate+1
+                continue
             col.append(abs(random.choice([ele for ele in range(rate-3,rate+3) if ele !=0 if ele <=10])))
             if(count%8==0):
                 rate=rate+1
