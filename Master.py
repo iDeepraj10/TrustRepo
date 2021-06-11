ccebf136b67b51e76117e3b8b8f316eba21f3751
Deepraj Chanda <30354189+iDeepraj10@users.noreply.github.com>
2021-06-11 20:38:36 +0530

    Update Master.py

--- Master.py
+++ Master.py
@@ -1,88 +1,12 @@
-
-
+###############################################################
+#####RATING PREDICTION IN THE CONTEXT OF SERVICE DISCOVERY#####
+###############################################################
 import pandas as pd
 import numpy as np
 from scipy import sparse
 from scipy.spatial import distance
-
-
-df = pd.read_csv( "C:\\Users\\dexter\\Desktop\\Trust and Reputation\\New folder\\Dataset\\matrix3.1.csv")
-df1=df
-
-
-
-
-df1= df1.fillna(0)
-#print(df1)
-
-
-#
-#def standardize(row):
-#    new_row = (row - row.mean())/(row.max()-row.min())
-#    return new_row
-
-#df_std = df1.apply(standardize)
-#print(df_std)
-
-
-
-#from sklearn.metrics.pairwise import cosine_similarity
-#from sklearn.metrics.pairwise import euclidean_distances
-#similarity= euclidean_distances(df_std)
-#print(similarity)
-
-
-
-#similarity_df = pd.DataFrame(similarity)
-#print(similarity_df)
-
-
-
-#def get_similar_consumer(c):
-#    sim_con= similarity_df[c]
-#    print(sim_con)
-#    print("similarity consumer : ")
-#    sim_con= sim_con.sort_values(ascending=True)
-#    return sim_con
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import euclidean_distances
 
 #function returns medium value of a service
 def median(service):
@@ -92,34 +16,63 @@ def median(service):
 def rating(customer,service):
     x = np.array(df)
     return x[custome][service]
-   
 
-#Customers with NaN values along with services
-x = np.argwhere(np.isnan(np.array(df)))
+def standardize(row):
+    new_row = (row - row.mean())/(row.max()-row.min())
+    return new_row    
+
+def get_similar_consumer(c):
+    sim_con= similarity_df[c]
+    sim_con= sim_con.sort_values(c,ascending=True)
+    return sim_con
 
 
 #function returns weight of a customer for specific service
 def weight(c,s):
-    Central_point = median(service)
+    Central_point = median(s)
     Rating = rating(c,s)
     Weight = (1 - abs((Central_point - Rating))/10)
     return Weight
 
+def predict(C,S):
+    W = weight(C,S)
+    R = rating(C,S)
+    M_rate = W * R 
+    return M_rate
+
+df = pd.read_csv( "C:\\Users\\dexter\\Desktop\\Trust and Reputation\\New folder\\Dataset\\matrix3.1.csv")
+df1=df
+
+
+df1= df1.fillna(0)
+#print(df1)
+
+
+#Customers with NaN values along with services
+M_ratings = np.argwhere(np.isnan(np.array(df)))
+
+
+#standardize the dataset 
+
+df_std = df1.apply(standardize)
+#print(df_std)
+
+#find similarity matrix
+similarity= euclidean_distances(df_std)
+#print(similarity)
 
 
 
+similarity_df = pd.DataFrame(similarity)
+#print(similarity_df)
+
+new_df = np.array(df1)
+for customer in M_ratings:
+    for service in customer:
+        temp = get_similar_consumer(customer)
+        print(temp)
+        #x = predict(temp[1],service)
+        #new_df[customer][service] = x    
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(pd.DataFrame(new_df))
